74d98cf473211d5517a636c5fcb6ea35d33915a7
aniket <aniket.hiware@sdaemon.co.in>
2024-07-10 21:39:46 +0530

    add user roles

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -14,6 +14,7 @@ export class AuthService {
   constructor(private http: HttpClient, private router: Router) {}
 
   login(username: string, password: string): Observable<any> {
+    debugger
     return this.http.get<any[]>(`${this.apiUrl}?username=${username}&password=${password}`)
       .pipe(tap(users => {
         if (users.length) {
